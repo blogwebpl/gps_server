02a43022734a3a89742da56076909d9419b28328
Tomasz Durałek <tomek@blogweb.pl>
2019-05-21 21:34:45 +0200

    add column

--- src/routes/collections.js
+++ src/routes/collections.js
@@ -6,7 +6,7 @@ import getColumns from '../functions/getColumns';
 
 export default (collectionName) => {
 	const getDocuments = asyncMiddleware(async(req, res) => {
-		const data = await User.find({}).select('name email').lean();
+		const data = await User.find({}).select('name email selectedRole').lean();
 		const response = {
 			data,
 			crud: 15,
