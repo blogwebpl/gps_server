c83b61217f9c483a35208f7271a3f276df238a6a
Tomasz Durałek <tomek@blogweb.pl>
2019-06-08 17:56:42 +0200

    post row path

--- src/routes/row.js
+++ src/routes/row.js
@@ -1,3 +1,4 @@
+import User from '../models/user';
 import asyncMiddleware from '../middleware/asyncMiddleware';
 import authenticate from '../middleware/authenticate';
 import express from 'express';
@@ -9,7 +10,20 @@ const getRow = asyncMiddleware(async(req, res) => {
 	};
 	res.send(response);
 });
+const postRow = asyncMiddleware(async(req, res) => {
+	const collectionName = req.params.collectionName;
+	const row = req.body.row;
+	try {
+		const answer = await User.updateOne({ _id: row._id }, { ...row });
+		console.log(answer);
+	} catch (err) {
+		res.sendStatus(400);
+	}
+	console.log(row);
+	res.sendStatus(200);
+});
 
 const router = new express.Router();
 router.get('/:collectionName', authenticate, getRow);
+router.post('/:collectionName', authenticate, postRow);
 export default router;
