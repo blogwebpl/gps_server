0f5f2e1d43707f4d17d3169c814bd0e14ead1f7c
Tomasz Durałek <tomek@blogweb.pl>
2019-06-08 18:52:16 +0200

    save data

--- src/routes/row.js
+++ src/routes/row.js
@@ -11,13 +11,21 @@ const getRow = asyncMiddleware(async(req, res) => {
 	res.send(response);
 });
 const postRow = asyncMiddleware(async(req, res) => {
-	const collectionName = req.params.collectionName;
+	// const collectionName = req.params.collectionName;
 	const row = req.body.row;
+	const _id = row._id;
 	try {
-		const answer = await User.updateOne({ _id: row._id }, { ...row });
-		console.log(answer);
+		if (_id) {
+			await User.updateOne({ _id }, { ...row });
+		} else {
+			const data = new User({
+				...row
+			});
+			await data.save();
+		}
 	} catch (err) {
-		res.sendStatus(400);
+		res.status(400).send(err.message);
+		return;
 	}
 	console.log(row);
 	res.sendStatus(200);
