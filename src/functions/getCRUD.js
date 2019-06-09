17e1a3e5753c370f9c16d380d6a9df4c553b8179
Tomasz Durałek <tomek@blogweb.pl>
2019-06-09 14:20:51 +0200

    get CRUD

--- src/functions/getCRUD.js
+++ src/functions/getCRUD.js
@@ -2,9 +2,9 @@ import getPermissionForCollection from '../functions/getPermissionForCollection'
 
 export default (user, collectionName) => {
 	const permission = getPermissionForCollection(user.selectedRole.permissions, collectionName);
-	const populate = permission ? permission.populate : {};
+	const crud = permission ? permission.crud : {};
 	try {
-		return JSON.parse((JSON.stringify(populate)));
+		return JSON.parse((JSON.stringify(crud)));
 	} catch (err) {
 		/* ignore coverage */
 		return {};
--- src/routes/collections.js
+++ src/routes/collections.js
@@ -1,21 +1,22 @@
 import asyncMiddleware from '../middleware/asyncMiddleware';
 import authenticate from '../middleware/authenticate';
 import express from 'express';
+import getCRUD from '../functions/getCRUD';
 import getColumns from '../functions/getColumns';
 import getModel from '../functions/getModel';
 import getPopulate from '../functions/getPopulate';
 
 export default (collectionName) => {
 	const getDocuments = asyncMiddleware(async(req, res) => {
-		// TODO: get crud
 		// TODO: test can user read
 		const columns = getColumns(req.user, collectionName);
 		const select = columns.map((column) => (column.key)).join(' ');
 		const populate = getPopulate(req.user, collectionName);
+		const crud = getCRUD(req.user, collectionName);
 		const data = await getModel(collectionName).find({}).select(select).populate(populate).lean();
 		const response = {
 			data,
-			crud: 15,
+			crud,
 			columns
 		};
 		res.send(response);
