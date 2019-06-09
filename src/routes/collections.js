66349631b0e9c9931ba60422c6f96d8a18453731
Tomasz Durałek <tomek@blogweb.pl>
2019-06-09 14:05:58 +0200

    get columns to select

--- src/routes/collections.js
+++ src/routes/collections.js
@@ -6,16 +6,16 @@ import getModel from '../functions/getModel';
 
 export default (collectionName) => {
 	const getDocuments = asyncMiddleware(async(req, res) => {
-		// TODO: change to dynamic collection name
-		// TODO: get fields to select
 		// TODO: get populate
 		// TODO: get crud
 		// TODO: test can user read
-		const data = await getModel(collectionName).find({}).select('name email selectedRole').populate({ path: 'selectedRole', select: 'name' }).lean();
+		const columns = getColumns(req.user, collectionName);
+		const select = columns.map((column) => (column.key)).join(' ');
+		const data = await getModel(collectionName).find({}).select(select).populate({ path: 'selectedRole', select: 'name' }).lean();
 		const response = {
 			data,
 			crud: 15,
-			columns: getColumns(req.user, collectionName)
+			columns
 		};
 		res.send(response);
 	});
