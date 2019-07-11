e691e4ea639b9bf10b1a4305eb73dae22efb8a7b
Tomasz Durałek <tomek@blogweb.pl>
2019-07-11 14:00:51 +0200

    fix code style

--- src/functions/getDictionaries.js
+++ src/functions/getDictionaries.js
@@ -7,13 +7,13 @@ import getModel from '../functions/getModel';
  */
 export default async(permission, collectionName) => {
 	/* ignore coverage */
-	const ds = (permission ? permission.fields.map((field) => (field.collectionName ? [ field.collectionName, field.key, field.collectionField ] : false)) : []);
+	const ds = (permission ? permission.fields.map((field) => (field.collectionName ? [field.collectionName, field.key, field.collectionField] : false)) : []);
 	const dictionaries = {};
 	await asyncForEach(ds, async(d) => {
 		if (d.length === 3) {
-			const collectionName = d[ 0 ];
-			const key = d[ 1 ];
-			const field = d[ 2 ];
+			const collectionName = d[0];
+			const key = d[1];
+			const field = d[2];
 			if (collectionName) {
 				const data = await getModel(collectionName).find(/* TODO: filter data with user filter if avaiable */).select(field).lean();
 				const dta = data.map((d) => ({
